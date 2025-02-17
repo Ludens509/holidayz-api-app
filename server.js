e6adb9eb281674e4143b0ce0c66a735ca4433241
Ludens509 <alexandreludens2@gmail.com>
2025-02-17 12:13:59 -0500

    17 FEB

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // Handle SPA routing (e.g., React Router)
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../holidayz-world/dist/index.html'));
+    res.sendFile(path.resolve(__dirname, '../', 'holidayz-world', 'dist', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
@@ -57,47 +57,3 @@ app.get("/api/holidays", async (req, res) => {
 app.listen(port, () => {
   console.log(`Proxy server running on http://localhost:${port}`);
 });
-// import path from "path";
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import fetch from "node-fetch";
-
-// dotenv.config();
-// const app = express();
-// const port = process.env.PORT || 5000;
-// const apikey = process.env.NODE_API_KEY;
-// app.use(cors());
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../holidayz-world.build')))
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, '../', 'holidayz-world', 'build', 'index.html')
-//     )
-//   )
-// }
-// else {
-//   app.get('/', (req, res) => res.send('Please set to production'))
-// }
-
-// app.get("/api/holidays", async (req, res) => {
-//   const { country, year, month, day } = req.query;
-//   let apiUrl = `https://holidayapi.com/v1/holidays?key=${apikey}&country=${country}&year=${year}`;
-
-//   if (month) apiUrl += `&month=${month}`;
-//   if (day) apiUrl += `&day=${day}`;
-
-//   try {
-//     const response = await fetch(apiUrl);
-//     const data = await response.json();
-//     res.json(data);
-//   } catch (error) {
-//     res.status(500).json({ error: "Failed to fetch data" });
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Proxy server running on http://localhost:${port}`);
-// });
